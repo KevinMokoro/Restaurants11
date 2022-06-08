4f9ac326ffe8623dd48d4bb0cddb8c1a47dddbed
KevinMokoro <klosvoke1@gmail.com>
2022-06-08 12:51:11 +0300

    This will create a basic UI and few intents.

--- app/src/main/java/com/moringaschool/myrestaurants11/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants11/MainActivity.java
@@ -2,13 +2,38 @@ package com.moringaschool.myrestaurants11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mFindRestaurantsButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if (v == mFindRestaurantsButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+            Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_LONG).show();
+        }
     }
 }
